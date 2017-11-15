402f02818fab9a7e24fd9f3a4695963ee3618f06
cmeschin <cedricmeschin@gmail.com>
2017-11-15 23:23:10 +0100

    v0.1 initialisation 	- exclusion du dossier .idea/ ajouté dans le .gitignore 	- initialisation du fichier module principal python_plugins.py

---
+++ python_plugins.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module principal pour les scripts de supervision
+
+Des classes seront créées au fur et à mesure des migrations de scripts actuellement en vbs et bash
+"""
+
+if __name__ == '__main__':
+    print("Module principal")

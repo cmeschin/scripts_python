d2de99778b1de6772b9ffc8b415a4d71d498ccbb
cmeschin <cedricmeschin@gmail.com>
2018-01-17 12:40:31 +0100

    2018-01-17 	- traitement du resultat pour extraire les statuts différents de RUNNING

--- check_supervisorctl.py
+++ check_supervisorctl.py
@@ -11,19 +11,19 @@ cmd = "sudo supervisorctl status"
 path = "/supervision/resultats/"
 #TODO: prévoir l'utilisation du chemin relatif pour une compatibilité avec les chemin Windows
 
-supervisorctl = "status_process_supervisorctl.txt"
+supervisorctl = "supervisorctl_status.txt"
 status_supervisorctl = (path + supervisorctl)
 
 #List the file in directory for delete file_status
 #TODO: Si le fichier xxxx.txt est dans le dossier /supervision/resultats/  alors le supprimer sinon lancer la commande.
-for root, dirs, files in os.walk(path):
-    for filename in files:
-        print(filename)
+ for root, dirs, files in os.walk(path):
+     for filename in files:
+         print(filename)
 
 os.remove(status_supervisorctl)
 
 #Launch the new file_status
-subprocess.run(cmd= $cmd, output= $path_$status_supervisorctl)
+#subprocess.run(cmd= $cmd, output= $path_$status_supervisorctl)
 
 #analyse du fichier status
 # exemple de retour pour logidoc
@@ -33,6 +33,18 @@ subprocess.run(cmd= $cmd, output= $path_$status_supervisorctl)
 #beemessenger:spool_beemessenger_batch_coriolis RUNNING    pid 12483, uptime 4:13:53
 #beemessenger:spool_beemessenger_batch_default RUNNING    pid 12472, uptime 4:13:53
 
-# boucle sur chaque ligne du fichier
-# découpage en trois colonnes (tabulations)
+# récupérer le fichier
+with open("C:/supervision/Scripts/resultats/supervisorctl_status.txt", "r") as file:
+    # boucle sur chaque ligne du fichier
+    for line in file:
+        ligne=line
+        #print("ligne lue:", ligne)
+        # découpage en trois colonnes (tabulations)
+        ligne=' '.join(ligne.split()) # supprime les espaces multiples de la chaine
+        print("ligne découpée:", ligne.split(" ",2)) # on ne traite que les deux premiers espaces
+        result=ligne.split(" ",2)
+        if result[1] != "RUNNING":
+            print("ALERTE !!!")
+        print("####################")
+
 # si Statut différent de RUNNING, on stocke les valeurs dans un tableau ERROR sinon dans un tab

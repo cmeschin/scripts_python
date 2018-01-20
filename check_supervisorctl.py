896c40572086a90615f34846f47a8f4e2097c482
cmeschin <cedricmeschin@gmail.com>
2018-01-20 21:06:27 +0100

    2018-01-20 v1    - ajout du module tessi_common incluant des méthodes standard de formatage des données    - epuration et refactorisation du code    - suppression des commentaires en surplus    - traitement du retour de supervisorctl directement dans une variable plutôt que dans un fichier    - optimisation des imports

--- check_supervisorctl.py
+++ check_supervisorctl.py
@@ -1,97 +1,82 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Launch the supervisorctl for view status process
+""" Launch the supervisorctl for view status process
 """
 
-import os, subprocess, time
+# import des modules
+import subprocess
 
-# definition des constantes
 from modules import centreon_status
+from modules import tessi_common
 
-maintenant = time.strftime("%Hh%M", time.gmtime())
+# definition des constantes "globales"
+# CMD = "sudo supervisorctl status"
+CMD = "cmd /c type d:\python\scripts_python\log\status.txt"
 
-#cmd = "sudo supervisorctl status"
-#cmd = "type d:\python\scripts_python\log\status.txt"
-cmd = "cmd /c type d:\python\scripts_python\log\status.txt"
-#path = "/supervision/resultats/"
-# TODO: prévoir l'utilisation du chemin relatif pour une compatibilité avec les chemins Windows
+# definition des variables "globales"
+detail = ""
+listError = ""
+listRunning = ""
+nbError = 0
+nbOk = 0
+nbProcess = 0
+now = tessi_common.define_now()
 
-supervisorctl = "supervisorctl_status.txt"
-status_supervisorctl = (path + supervisorctl)
-
-# List the file in directory for delete file_status and Launch for new file_status
-# TODO: Commande suppression OK ==>A tester lancer la commande.
-from pathlib import Path
-
-# if os.path.exists(status_supervisorctl):
-#     os.remove(status_supervisorctl)
-# # Tester la commande.
-# else:
-    #subprocess.run(cmd= $cmd, output= $path_$status_supervisorctl)
-retour = subprocess.run(cmd, stdout=subprocess.PIPE)
-#print("retour=" + str(retour.stdout))
+# execution de la commande et récupération du retour dans une variable
+actualState = subprocess.run(CMD, stdout=subprocess.PIPE)
 
 # analyse du fichier status
 # exemple de retour pour logidoc
-# nom_process                                  |Etat      |message
+# nom_process                                   |Etat      |message
 # beemessenger:prod_logidoc_beemessenger_esendex RUNNING    pid 12480, uptime 4:13:53
 # beemessenger:prod_logidoc_beemessenger_repriserg RUNNING    pid 12474, uptime 4:13:53
 # beemessenger:spool_beemessenger_batch_coriolis RUNNING    pid 12483, uptime 4:13:53
 # filebeat                         STOPPED    Not started
 # beemessenger:spool_beemessenger_batch_default RUNNING    pid 12472, uptime 4:13:53
 
-# ouvrir le fichier
-#with open("D:/Python/scripts_python/log/status.txt", "r") as file:
-    # boucle sur chaque ligne du fichier
-nbProcess = 0
-nbError = 0
-nbOk = 0
-list_running = ""
-list_error = ""
-detail = ""
-#print("############## ligne:" + str(retour.stdout)[2:-1])
-retour=str(retour.stdout)[2:-1].split("\\r\\n")
-for ligne in retour:
+#    boucle sur chaque ligne du fichier
+# print("############## ligne:" + str(retour.stdout)[2:-1])
+lstActualState = str(actualState.stdout)[2:-1].split("\\r\\n")
+for line in lstActualState:
     nbProcess += 1  # comptage du nombre de process configurés
-    print("ligne lue:", ligne)
+    print("ligne lue:", line)
     # découpage en trois colonnes (tabulations)
-    ligne = ' '.join(ligne.split())  # supprime les espaces multiples de la chaine
-    print("ligne découpée:", ligne.split(" ", 2))  # on ne traite que les deux premiers espaces
-    result = ligne.split(" ", 2)
-    # si Statut différent de RUNNING, on stock les valeurs dans un tableau list_error sinon dans list_running
+    line = ' '.join(line.split())  # supprime les espaces multiples de la chaine
+    print("ligne découpée:", line.split(" ", 2))  # on ne traite que les deux premiers espaces
+    result = line.split(" ", 2)
+    # si Statut différent de RUNNING, on stock les valeurs dans un tableau listError sinon dans listRunning
     if result[1] != "RUNNING":
         print("ALERTE !!!")
         # ajout dans la variable ERROR la liste des process arrêtés
-        list_error += "{} {} {}\n".format(result[0],result[1],result[2])
+        listError += "{} {} {}\n".format(result[0], result[1], result[2])
         nbError += 1  # Comptage du nombre d'erreurs
     else:
         print("OK")
         # ajout dans la variable RUNNING la liste des process lancés
-        list_running += "{} {} {}\n".format(result[0],result[1],result[2])
+        listRunning += "{} {} {}\n".format(result[0], result[1], result[2])
         nbOk += 1  # Comptage du nombre de process lancés
     print("####################")
 
 # on traite les résultats
-# si des éléments sont contenus dans la liste list_error, on sort un statut CRITIQUE
+# si des éléments sont contenus dans la liste listError, on sort un statut CRITIQUE
 if nbError == nbProcess:
     # Si tous les process sont arrêtés
     status = "CRITICAL"
-    message = "Tous les process sont arrêtés (" + str(nbError) + "/" + str(nbProcess) + ") à " + maintenant + "!"
-    detail = "Process KO:\n" + list_error
+    message = "Tous les process sont arrêtés (" + str(nbError) + "/" + str(nbProcess) + ") à " + now + "!"
+    detail = "Process KO:\n" + listError
 elif not nbError <= 0 & nbProcess > nbError:
     # s'il y a des erreurs mais en nombre inférieur au nombre total de process => WARNING
     status = "WARNING"
-    message = str(nbError) + " process sur " + str(nbProcess) + " arrêté(s) à " + maintenant + "!"
-    detail = "Process KO:\n" + list_error + "Process OK:\n" + list_running
+    message = str(nbError) + " process sur " + str(nbProcess) + " arrêté(s) à " + now + "!"
+    detail = "Process KO:\n" + listError + "Process OK:\n" + listRunning
 elif nbOk == nbProcess:
     status = "OK"
-    message = "Tous les process sont démarrés (" + str(nbOk) + "/" + str(nbProcess) + ") à " + maintenant + "."
-    detail = "Process OK:\n" + list_running
+    message = "Tous les process sont démarrés (" + str(nbOk) + "/" + str(nbProcess) + ") à " + now + "."
+    detail = "Process OK:\n" + listRunning
 else:
     status = "UNKNOWN"
     message = "Cas non géré: nbProcess=" + str(nbProcess) + "; nbError=" + str(nbError) + "; nbOK=" + str(nbOk) + \
-    " a " + maintenant + ". Contacter l'équipe centreon (centreon_tt@tessi.fr) pour analyse. "
+              " a " + now + ". Contacter l'équipe centreon (centreon_tt@tessi.fr) pour analyse. "
 
 centreon_status.exit(status, message, detail)
---
+++ modules/tessi_common.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+import sys
+import time
+
+"""
+    Module de gestion des classes et méthodes communes aux script tessi
+    date, répertoire de résultats, etc...
+    consulter http://strftime.org/ pour les format possibles
+"""
+
+
+def define_result_folder():
+    """Méthode de définition du répertoire de stockage des résultats et fichiers temporaires
+        selon la plateforme (Windows ou Linux)
+    """
+    if sys.platform == "win32":
+        return "C:/supervision/scripts/resultats/"
+    else:
+        return "/supervision/resultats/"
+
+
+def define_now(format="%Hh%M"):
+    """ Méthode permettant de retourner une chaine de l'heure actuelle en fonction du format indiqué
+        :param format: format de l'heure attendue (par défaut %Hh%M)
+    """
+    now = time.strftime(format, time.gmtime())
+    return now
+
+
+def define_today(format="%d/%m/%Y"):
+    """ Méthode permettant de retourner une chaine de la date actuelle en fonction du format indiqué
+        :param format: format de l'heure attendue (par défaut %d/%m/%Y)
+    """
+    today = datetime.datetime.now().strftime(format)
+    return today
+
+
+def define_day(format="%A"):
+    """ Méthode permettant de retourner une chaine du jour de la semaine en fonction du format indiqué
+        :param format: format de l'heure attendue (par défaut %A => like Monday)
+    """
+    dayofweek = datetime.datetime.now().strftime(format)
+    return dayofweek
+
+
+def define_month(format="%B"):
+    """ Méthode permettant de retourner une chaine du mois en cours en fonction du format indiqué
+        :param format: format de l'heure attendue (par défaut %B => like September)
+    """
+    month = datetime.datetime.now().strftime(format)
+    return month
+
+
+def define_year(format="%Y"):
+    """ Méthode permettant de retourner une chaine de l'année en cours en fonction du format indiqué
+            :param format: format de l'heure attendue (par défaut %Y => like 2018)
+        """
+    year = datetime.datetime.now().strftime(format)
+    return year
+
+
+if __name__ == "__main__":
+    print("-- Module de gestion des classes et méthodes communes aux scripts tessi (format de date, "
+          "répertoire de résultats, etc... --")
+    print(sys.argv[1:])

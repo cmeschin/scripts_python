771efd2e5fd40cc54e7444f249e8eae674400481
cmeschin <c.meschin@free.fr>
2020-02-24 11:20:43 +0100

    2020-02-24 version 4: Gestion des clusters

--- check_riverbed_api.py
+++ check_riverbed_api.py
@@ -7,7 +7,7 @@
     2020-01-24 version 1: version initiale
     2020-02-12 version 2: Ajout mode flow
     2020-02-14 version 3: correction gestion fichier pickle
-
+    2020-02-24 version 4: Gestion des clusters
     TODO: variabiliser user/mdp
 """
 
@@ -23,12 +23,14 @@ import pickle
 import requests
 
 # definition des constantes
-VERSION = "2020-02-14 version 3"
+VERSION = "2020-02-24 version 4"
 USERNAME = "publicring"
 PASSWORD = "nd7ja*!ja"
 WORK_DIR = tessi_common.define_result_folder()
 STAT_FLOW = "riverbed_stat_flow.pkl"
 URL_RIVERBED = "https://riverbed-gateway.interne.tessi-techno.fr/api/gateway/1.5/stats"
+# CLUSTERS = {"CLUSTER1": ['10.138.101.10', '10.33.253.1'], "CLUSTER2": ['10.33.253.221', '10.33.253.222']}  # pour test
+CLUSTERS = {"CLUSTER1": ['10.33.253.1', '10.33.253.2'], "CLUSTER2": ['10.33.253.221', '10.33.253.222']}
 
 def stat_flow(exclude):
     nbErrors = 0
@@ -36,10 +38,10 @@ def stat_flow(exclude):
     # sys.exit(0)
 
     # vérification de l'existence du fichier pickle
-    dict = {}
+    dictIpErrors = {}
     if os.path.exists(WORK_DIR+STAT_FLOW):
         try:
-            dict = pickle.load(open(WORK_DIR+STAT_FLOW, 'rb'))
+            dictIpErrors = pickle.load(open(WORK_DIR+STAT_FLOW, 'rb'))
         except Exception as err:
             print("ERREUR: Impossible d'ouvrir le fichier de stat {} ({})".format(WORK_DIR+STAT_FLOW, err))
             sys.exit(3)
@@ -48,72 +50,173 @@ def stat_flow(exclude):
     r = requests.get(URL_RIVERBED, auth=(USERNAME, PASSWORD))
 
     # reformatage du résultat par ligne
-    result = r.text.split( "\n")
-    list_update = []
-    fullline = {}
+    result = r.text.split("\n")  # retourne une liste de lignes
+    # print(result)
+    list_update = []  # crée une liste pour la mise à jour des ip
+    fullline = {}  # crée un dictionnaire pour les lignes complètes avec l'ip comme clé
     # parcourir le résultat et stocker les valeurs de chaque ligne
     for line in result:
         flow = ""
-        ipaddr = ""
+        ipAddr = ""
         newLine = ""
+        isCluster = False
+        ipCluster = ""
 
         if "<flow_source flow_type" in line:
-            values = line.split(" ")
-            # check ipaddr
+            values = line.split(" ")  # liste des valeurs de la ligne ('flow_type="SFlow"', 'flows_received_last_min="5"', 'ipaddr="10.62.101.10"', ...)
+
+            for ips in CLUSTERS.values():
+                # print("cluster: {}".format(ips))
+                for ip in ips:
+                    if ip in line:
+                        # print("IP cluster: {}".format(ip))
+                        isCluster = True
+                        ipCluster = ip
+
             # si ce n'est pas les clusters
-            if "10.33.253.1" not in line and "10.33.253.2" not in line and "10.33.253.221" not in line and "10.33.253.222" not in line:
+            if not isCluster:
+            # if "10.33.253.1" not in line and "10.33.253.2" not in line and "10.33.253.221" not in line and "10.33.253.222" not in line:
                 for value in values:
-                    updated = False
+                    # updated = False
                     myvalue = value.split("=")
-                    #print("myvalue:{}".format(myvalue))
+                    # print("myvalue:{}".format(myvalue))
                     # reconstruction de la ligne
                     newLine += " " + value
                     if myvalue[0] == "flows_received_last_min":
-                        flow=int(value.split("=")[1].replace('"',''))
+                        flow = int(value.split("=")[1].replace('"',''))
                     elif myvalue[0] == "ipaddr":
-                        ipaddr=value.split("=")[1].replace('"','')
-                        list_update.append(ipaddr)
+                        ipAddr = value.split("=")[1].replace('"','')
+                        list_update.append(ipAddr)
 
-                    #print("Liste update1: {}".format(list_update))
-                    if flow != "" and ipaddr != "" and updated == True:
-                        updated = False
-                        #print("flow:{} {}".format(flow,ipaddr))
+                    # # print("Liste update1: {}".format(list_update))
+                    # if flow != "" and ipAddr != "" and updated == True:
+                    #     updated = False
+                    #     #print("flow:{} {}".format(flow,ipAddr))
 
                 # mise en forme de la chaine (suppression de la balise <flow source .../>)
-                fullline[ipaddr] = newLine.strip()[13:-2]
+                fullline[ipAddr] = newLine.strip()[13:-2]
                 # Ajout de l'IP dans le dictionnaire si elle n'existe pas déjà
-                if ipaddr not in dict:
-                    #print("Ajout de l'adresse {}.".format(ipaddr))
-                    dict[ipaddr] = 0
+                if ipAddr not in dictIpErrors:
+                    # print("Ajout de l'adresse {}.".format(ipAddr))
+                    dictIpErrors[ipAddr] = 0
 
-
-                if flow == 0 and ipaddr not in exclude:
+                if flow == 0 and ipAddr not in exclude:
                     # si pas de flow, on ajoute/incrémente dans le dictionnaire
-                    # print("ipaddr:{}".format(ipaddr))
+                    # print("ipAddr:{}".format(ipAddr))
                     # print("exclude: {}".format(exclude))
-                    nbcheck = dict.get(ipaddr)+1
+                    nbcheck = dictIpErrors.get(ipAddr)+1
                     # print("nbcheck:{}".format(nbcheck))
-                    dict[ipaddr] = nbcheck
-                    # print("dict:{}".format(dict[ipaddr]))
+                    dictIpErrors[ipAddr] = nbcheck
+                    # print("ipErrors:{}".format(ipErrors[ipAddr]))
                 else:
                     nbcheck = 0
-                    dict[ipaddr] = nbcheck
+                    dictIpErrors[ipAddr] = nbcheck
+            else:  # c'est un cluster
+                for value in values:
+                    # updated = False
+                    myvalue = value.split("=")
+                    # print("myvalue:{}".format(myvalue))
+                    # reconstruction de la ligne
+                    newLine += " " + value
+                    if myvalue[0] == "flows_received_last_min":
+                        flow = int(value.split("=")[1].replace('"',''))
+                    elif myvalue[0] == "ipaddr":
+                        ipAddr = value.split("=")[1].replace('"','')
+                        list_update.append(ipAddr)
+
+                    # # print("Liste update1: {}".format(list_update))
+                    # if flow != "" and ipAddr != "" and updated == True:
+                    #     updated = False
+                    #     #print("flow:{} {}".format(flow,ipAddr))
+
+                    # mise en forme de la chaine (suppression de la balise <flow source .../>)
+                    myCluster = ""
+                    for cluster, ips in CLUSTERS.items():
+                        if ipAddr in ips:
+                            myCluster = cluster
+
+                fullline[ipAddr] = "Cluster {}: {}".format(myCluster, newLine.strip()[13:-2])
+                # Ajout de l'IP dans le dictionnaire si elle n'existe pas déjà
+                if ipAddr not in dictIpErrors:
+                    # print("Ajout de l'adresse {}.".format(ipAddr))
+                    dictIpErrors[ipAddr] = 0
+
+                if flow == 0 and ipAddr not in exclude:
+                    # si pas de flow, on ajoute/incrémente dans le dictionnaire
+                    nbcheck = dictIpErrors.get(ipAddr)+1
+                    # print("nbcheck:{}".format(nbcheck))
+                    dictIpErrors[ipAddr] = nbcheck
+                    # print("ipErrors:{}".format(ipErrors[ipAddr]))
+                    # Récupération IP secondaire
+                    for cluster, ips in CLUSTERS.items():
+                        if ipAddr in ips and ipAddr != ips[0]:
+                            ipAddr2 = ips[0]
+                        else:
+                            ipAddr2 = ips[1]
+
+                    for line in result:
+                        if ipAddr2 in line:
+                            myLine = result.index(line)  # recupère l'index de la ligne contenant l'ipAddr2
+                            values = result[myLine].split(" ")
+
+                    for value in values:
+                        # updated = False
+                        myvalue = value.split("=")
+                        # print("myvalue:{}".format(myvalue))
+                        # reconstruction de la ligne
+                        newLine += " " + value
+                        if myvalue[0] == "flows_received_last_min":
+                            flow = int(value.split("=")[1].replace('"',''))
+                        elif myvalue[0] == "ipaddr":
+                            ipAddr = value.split("=")[1].replace('"','')
+                            list_update.append(ipAddr)
+
+                    # # print("Liste update1: {}".format(list_update))
+                    # if flow != "" and ipAddr != "" and updated == True:
+                    #     updated = False
+                    #     #print("flow:{} {}".format(flow,ipAddr))
+
+                    # mise en forme de la chaine (suppression de la balise <flow source .../>)
+                    fullline[ipAddr] = newLine.strip()[13:-2]
+                    # Ajout de l'IP dans le dictionnaire si elle n'existe pas déjà
+                    if ipAddr not in dictIpErrors:
+                        # print("Ajout de l'adresse {}.".format(ipAddr))
+                        dictIpErrors[ipAddr] = 0
+
+                    if flow == 0 and ipAddr not in exclude:
+                        # si pas de flow, on ajoute/incrémente dans le dictionnaire
+                        # print("ipAddr:{}".format(ipAddr))
+                        # print("exclude: {}".format(exclude))
+                        nbcheck = dictIpErrors.get(ipAddr)+1
+                        # print("nbcheck:{}".format(nbcheck))
+                        dictIpErrors[ipAddr] = nbcheck
+                        # print("ipErrors:{}".format(ipErrors[ipAddr]))
+                    else:
+                        nbcheck = 0
+                        dictIpErrors[ipAddr] = nbcheck
+
+
+                else:
+                    nbcheck = 0  # reinit du compteur
+                    dictIpErrors[ipAddr] = nbcheck
+
     # Nettoyage du dictionnaire des ip qui n'existe plus
     del_ip = []
     # print("Liste update: {}".format(list_update))
-    for ip in dict.keys():
+    for ip in dictIpErrors.keys():
         if ip not in list_update:
-            #print("IP: {}".format(ip))
+            # print("IP: {}".format(ip))
             del_ip = list.append("{}".format(ip))
             # print("Suppression de l'ip {}".format(ip))
 
+    # Mise à jour du dictionnaire
     for ip in del_ip:
-        del dict[ip]
+        del dictIpErrors[ip]
         # print("Suppression de l'IP {} dans le dictionnaire.".format(ip))
 
     # sauvegarde du résultat dans le fichier pickle
     try:
-        pickle.dump(dict, open(WORK_DIR+STAT_FLOW, 'wb'))
+        pickle.dump(dictIpErrors, open(WORK_DIR+STAT_FLOW, 'wb'))
     except IOError as err:
         print("ERREUR: Impossible d'ecrire le fichier stat {} ({}).".format(WORK_DIR+STAT_FLOW, err))
         sys.exit(3)
@@ -121,13 +224,32 @@ def stat_flow(exclude):
     # analyse du résultat
     # si une ou plusieurs IP sont à trois check ou plus on remonte l'alerte
     ipErrors = {}
-    for cle,valeur in dict.items():
+
+    for ip, nbErr in dictIpErrors.items():
+        isCluster = False
+        indexCluster = ""
         # print("Cle: {}, Valeur: {}, ipErrors: {}".format(cle, valeur,ipErrors))
-        if valeur >= 3:
+        for cluster, ips in CLUSTERS.items():
+            if ip in ips:
+                isCluster = True
+                indexCluster = cluster
+
+        if not isCluster and nbErr >= 3:  # pas cluster et erreurs
             # print("Cle {}".format(cle))
-            ipErrors[cle] = fullline[cle]
-            nbErrors = nbErrors+1
-            # print("Ajout Error: {}".format(ipErrors[cle]))
+            ipErrors[ip] = fullline[ip]
+            nbErrors = nbErrors+1  # nombre de ligne en erreur
+            # print("Ajout Error: {}".format(ipErrors[ip]))
+        elif isCluster:  # cluster
+            myClusterErr = False
+            for ips in CLUSTERS[indexCluster]:
+                # print("Value IndexCLuster {}: {}".format(indexCluster, ips))
+                if dictIpErrors[ips] >= 3:
+                    myClusterErr = True
+                    ipErrors[ips] = fullline[ips]
+                else:
+                    myClusterErr = False
+            if myClusterErr == True:
+                nbErrors = nbErrors+1
 
     if nbErrors >= 1:
         status = "CRITICAL"
